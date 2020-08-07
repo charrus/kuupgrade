2dba8e96152fa88baf7cc10f2f69bd0dbe30bae5
charrus <charlie@rusbridger.com>
2020-08-07 19:53:51 +0100

    Add install, remove. Make LinuxKernel sortable

--- src/LinuxKernels.py
+++ src/LinuxKernels.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python3
 
 import apt_pkg
-import multiprocessing as mp
-import pprint
+import os.path
 import re
 import requests
 import requests_cache
 import subprocess
+import tempfile
+
+# Parse the main page for links to versions
+re_index = re.compile(r'<a href="(v[a-zA-Z0-9\-._]+\/)">([a-zA-Z0-9\-._]+)\/<\/a>')
+
+# Extract all the links in the version page
+re_urls = re.compile(r'<a href="[a-zA-Z0-9\-._\/]+">[a-zA-Z0-9\-._\/]+\.deb<\/a>')
+
+# Extract a single package parts
+re_all = re.compile(r'<a href="(?P<uri>[a-zA-Z0-9\-._/]+)">'+
+                     r'(?P<dir>[a-z0-9]+/)?'+
+                     r'(?P<package>[a-zA-Z0-9\-.]+)_'+
+                     r'(?P<version>[a-zA-Z0-9\-.\/]+)_'+
+                     r'(?P<arch>[a-zA-Z0-9\-\/]+)\.deb</a>')
+
+# Extract numeric and optional text from versions
+re_version = re.compile(r'(\w)?(?P<vers>\d+)(?:-(?P<label>[^-]*)(-(.*)))?')
+
 
 def split_version(version):
+    """
+    Split the version into major, minor, subminor
+    """
+
     sub_versions = version.split('.')
     major = sub_versions[0]
     sub_minor = "0"
@@ -20,9 +41,15 @@ def split_version(version):
     return major,minor,sub_minor
 
 def numeric_version(version):
-    major,minor,subminor = split_version(version)
+    """
+    Calculate a numeric form of the version - roughly:
 
-    re_version = re.compile(r'(\w)?(?P<vers>\d+)(?:-(?P<label>[^-]*)(-(.*)))?')
+    major * 1000000 + minor * 1000 + subminor
+
+    Release candidates subtract 500
+    """
+
+    major,minor,subminor = split_version(version)
 
     m = re_version.match(major)
     numeric = int(m.group("vers"))
@@ -52,54 +79,90 @@ class LinuxKernel:
         if version.find("rc"):
             self.rc = True
         self.numeric_version = numeric_version(version)
-
-    def init(self):
-        REX = re.compile(r'<a href="([a-zA-Z0-9\-._\/]+)">([a-zA-Z0-9\-._\/]+)<\/a>')
-        REX_HEADER_ALL = re.compile(r'[a-zA-Z0-9\-._\/]*linux-headers-[a-zA-Z0-9.\-_]*_all.deb')
-        REX_HEADER = re.compile(r'[a-zA-Z0-9\-._\/]*linux-headers-[a-zA-Z0-9.\-_]*generic_[a-zA-Z0-9.\-]*_' + self.arch + r'.deb')
-        REX_IMAGE = re.compile(r'[a-zA-Z0-9\-._\/]*linux-image-[a-zA-Z0-9.\-_]*generic_([a-zA-Z0-9.\-]*)_' + self.arch + r'.deb')
-        REX_IMAGE_EXTRA = re.compile(r'[a-zA-Z0-9\-._\/]*linux-image-extra-[a-zA-Z0-9.\-_]*generic_[a-zA-Z0-9.\-]*_' + self.arch + r'.deb')
-        REX_MODULES = re.compile(r'[a-zA-Z0-9\-._\/]*linux-modules-[a-zA-Z0-9.\-_]*generic_[a-zA-Z0-9.\-]*_' + self.arch + r'.deb')
-
-        # Extract the urls for our architecture
-        # Especially for the all debs, these are listed multiple times
-        # per architecture, so use a dict to make them unique
-        urls = {}
-
-        # Grab the version page
-        rver = requests.get(self.url)
-        # The version can be extracted from the .deb filenames
-        deb_versions = REX_IMAGE.findall(rver.text)
-        # If we can't extract the version, then skip it
-        if not deb_versions:
-            raise LookupError("Unable to find any versions")
-
-        self.deb_version = deb_versions[0]
-
-        for debs in REX.findall(rver.text):
-            # First group is the uri - so add the base url
-            file_url = rver.url + debs[0]
-            # Second group is the embedded version in the deb filename
-            file_name = debs[1]
-
-            if REX_HEADER.match(file_name):
-                urls[file_url] = 1
-            elif REX_HEADER_ALL.match(file_name):
-                urls[file_url] = 1
-            elif REX_IMAGE.match(file_name):
-                urls[file_url] = 1
-            elif REX_IMAGE_EXTRA.match(file_name):
-                urls[file_url] = 1
-            elif REX_MODULES.match(file_name):
-                urls[file_url] = 1
-            else:
-                continue
-
-        self.urls = list(urls.keys())
-        self.valid = True
+        self.packages = {}
 
     def __str__(self):
         return self.version
+
+    def __eq__(self, other):
+        return self.numeric_verion == other.numeric_version
+
+    def __lt__(self, other):
+        return self.numeric_version < other.numeric_version
+
+    def init(self):
+        # Grab the version page
+        rver = requests.get(self.url)
+
+        for debs in re_urls.findall(rver.text):
+            m = re_all.match(debs)
+            if not m:
+                continue
+
+            if m.group('arch') not in [self.arch, "all"]:
+                continue
+
+            package_name = m.group('package')
+
+            # Assume that the last bit of the package name is a word, rather
+            # than numbers then this is the flavour of kernel, usually
+            # generic or lowlatency
+            flavour = package_name.split("-")[-1]
+            if not flavour.isalpha():
+                flavour = None
+            package = {}
+            package['url'] = rver.url + m.group('uri')
+            package['arch'] = m.group('arch')
+            package['version'] = m.group('version')
+            package['dir'] = m.group('dir')
+            package['flavour'] = flavour
+            self.packages[package_name] = package
+            self.deb_version = m.group('version')
+
+        if not self.packages:
+            raise LookupError("Unable to find any versions")
+
+
+    def install(self, flavour="generic"):
+        """Install the packages for this version
+           flavour can be lowlatency, generic etc"""
+
+        with requests_cache.disabled():
+            with tempfile.TemporaryDirectory() as tmpdir:
+                files_to_install = []
+
+                for package in self.packages.keys():
+                    pkgflavour = self.packages[package]['flavour']
+                    if pkgflavour and pkgflavour != flavour:
+                        continue
+                    filename = self.packages[package]['url'].split("/")[-1]
+                    fqfilename = os.path.join(tmpdir,filename)
+                    print("Downloading "+self.packages[package]['url'])
+                    r = requests.get(self.packages[package]['url'])
+                    with open(fqfilename, "wb") as f:
+                        f.write(r.content)
+                        files_to_install.append(fqfilename)
+
+                command = ['apt-get', 'install'] + files_to_install
+                print("Running: "+" ".join(command))
+                result = subprocess.run(command, stdout=subprocess.PIPE)
+
+    def remove(self, flavour="generic"):
+        """Remove the packages for this version
+           flavour can be lowlatency, generic etc"""
+
+        packages_to_rm = []
+
+        for package in self.packages.keys():
+            pkgflavour = self.packages[package]['flavour']
+            if pkgflavour and pkgflavour != flavour:
+                continue
+            packages_to_rm.append(package)
+
+        command = ['apt-get', 'remove'] + packages_to_rm
+        print("Running: "+" ".join(command))
+        result = subprocess.run(command, stdout=subprocess.PIPE)
+
 
 class LinuxKernels:
     """All Kernel on mainline"""
@@ -135,8 +198,6 @@ class LinuxKernels:
         with requests_cache.disabled():
             r = requests.get(self.URI_KERNEL_UBUNTU_MAINLINE)
 
-        re_index = re.compile(r'<a href="(v[a-zA-Z0-9\-._]+\/)">([a-zA-Z0-9\-._]+)\/<\/a>')
-
         for line in re_index.findall(r.text):
             version = line[1]
             if numeric_version(version) < numeric_min_version:
@@ -156,7 +217,7 @@ class LinuxKernels:
             print(".", end="", flush=True)
 
     def version(self, version):
-        for kernel in kernels:
+        for kernel in self.kernels:
             if kernel.version == version:
                 return kernel
 
--- src/kuupgrade.py
+++ src/kuupgrade.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 
 import argparse
+import pprint
 
 from LinuxKernels import LinuxKernels
 
 parser = argparse.ArgumentParser(description='Upgrade ubuntu kernel')
-parser.add_argument('--list', action='store_true', default=True, help='List available kernels')
+parser.add_argument('--list', action='store_true', help='List available kernels')
+parser.add_argument('--install', help='Install kernel')
+parser.add_argument('--remove', help='Remove kernel')
+parser.add_argument('--info', help='Kernel info')
 args = parser.parse_args()
 
-print("Initializing...")
 kernels = LinuxKernels()
+print("Initializing...")
 kernels.init()
 print("Done.")
 
@@ -18,12 +22,23 @@ if args.list:
         running = ""
         installed = ""
 
-        if kernel.valid:
-            if kernel.running:
-                running = "[Running]"
-            if kernel.installed:
-                installed = "[Installed]"
-            version = kernel.version
+        if kernel.running:
+            running = "[Running]"
+        if kernel.installed:
+            installed = "[Installed]"
+        version = kernel.version
+        deb_version = kernel.deb_version
 
-            print(f'{version:10} {running} {installed}');
+        print(f'{version:20} {deb_version:40} {running} {installed}');
 
+if args.install:
+    kernel = kernels.version(args.install)
+    kernel.install()
+
+if args.remove:
+    kernel = kernels.version(args.remove)
+    kernel.remove()
+
+if args.info:
+    kernel = kernels.version(args.info)
+    pprint.pprint(kernel.packages)
